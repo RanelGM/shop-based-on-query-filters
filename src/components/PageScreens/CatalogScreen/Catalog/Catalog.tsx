f70b3e69558540b5f595273bafdc353addd1d880
RanelGM <mr.rr.coon@gmail.com>
2022-09-06 23:19:21 +0300

    Move Filter component inside Catalog component

--- src/components/PageScreens/CatalogScreen/Catalog/Catalog.tsx
+++ src/components/PageScreens/CatalogScreen/Catalog/Catalog.tsx
@@ -3,8 +3,8 @@ import { useViewport } from "hooks/useViewport";
 import { Button } from "components/Common/Button/Button";
 import { Icon } from "components/Common/Icon/Icon";
 import { FilterSortModal } from "components/Modals/FilterSortModal/FilterSortModal";
-import { Filter } from "../Filter/Filter";
 import { Pagination } from "../Pagination/Pagination";
+import { Filter } from "./Filter/Filter";
 import { GuitarCard } from "./GuitarCard/GuitarCard";
 import { Sort } from "./Sort/Sort";
 import style from "./Catalog.module.scss";
@@ -12,10 +12,9 @@ import style from "./Catalog.module.scss";
 const mockGuitarCount = 9;
 
 export const Catalog = () => {
-  const { isMobileViewport, isTabletViewport } = useViewport();
+  const { isDesktopViewport, isMobileViewport } = useViewport();
   const [isFilterModalOpen, filterModalCallbacks] = useModal({});
   const [isSortModalOpen, sortModalCallbacks] = useModal({});
-  const isFilterButtonShow = isTabletViewport || isMobileViewport;
 
   return (
     <>
@@ -31,34 +30,38 @@ export const Catalog = () => {
       )}
 
       <section className={style.component}>
-        <div className={style.filtersWrapper}>
-          {isFilterButtonShow && (
-            <Button
-              icon={<Icon iconName="filter" />}
-              className={style.filterButton}
-              color="white-black"
-              onClick={filterModalCallbacks.openModal}
-            >
-              Фильтры
-            </Button>
-          )}
+        {isDesktopViewport && <Filter />}
 
-          {isMobileViewport ? (
-            <Button className={style.filterButton} color="white-black" onClick={sortModalCallbacks.openModal}>
-              Сортировка
-            </Button>
-          ) : (
-            <Sort />
-          )}
+        <div className={style.catalogWrapper}>
+          <div className={style.filtersWrapper}>
+            {!isDesktopViewport && (
+              <Button
+                icon={<Icon iconName="filter" />}
+                className={style.filterButton}
+                color="white-black"
+                onClick={filterModalCallbacks.openModal}
+              >
+                Фильтры
+              </Button>
+            )}
+
+            {isMobileViewport ? (
+              <Button className={style.filterButton} color="white-black" onClick={sortModalCallbacks.openModal}>
+                Сортировка
+              </Button>
+            ) : (
+              <Sort />
+            )}
+          </div>
+          <ul className={style.guitarsList}>
+            {Array.from({ length: mockGuitarCount }, (item, index) => (
+              <li key={index}>
+                <GuitarCard />
+              </li>
+            ))}
+          </ul>
+          <Pagination />
         </div>
-        <ul className={style.guitarsList}>
-          {Array.from({ length: mockGuitarCount }, (item, index) => (
-            <li key={index}>
-              <GuitarCard />
-            </li>
-          ))}
-        </ul>
-        <Pagination />
       </section>
     </>
   );
--- src/components/PageScreens/CatalogScreen/CatalogScreen.tsx
+++ src/components/PageScreens/CatalogScreen/CatalogScreen.tsx
@@ -1,20 +1,12 @@
-import { useViewport } from "hooks/useViewport";
 import { Positioner } from "components/Common/Positioner/Positioner";
 import { Catalog } from "./Catalog/Catalog";
-import { Filter } from "./Filter/Filter";
 import style from "./CatalogScreen.module.scss";
 
 export const CatalogScreen = () => {
-  // В случае tablet, mobile viewport Фильтр будет внутри коммпонента Catalog
-  const { isDesktopViewport } = useViewport();
-
   return (
     <Positioner>
       <h1 className={style.heading}>Каталог гитар</h1>
-      <div className={style.catalogWrapper}>
-        {isDesktopViewport && <Filter />}
-        <Catalog />
-      </div>
+      <Catalog />
     </Positioner>
   );
 };
