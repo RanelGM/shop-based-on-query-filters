b7bb24b6622b3c5d6fc2dc199ff64069bb8e9c5a
RanelGM <mr.rr.coon@gmail.com>
2022-09-15 22:51:18 +0300

    Add cart modal component

---
+++ src/components/Modals/CartModal/CartModal.tsx
@@ -0,0 +1,59 @@
+import { MutableRefObject } from "react";
+import { Guitar } from "types/guitar";
+import { Button } from "components/Common/Button/Button";
+import { ButtonClose } from "components/Common/Button/ButtonClose";
+import { Overlay } from "components/Common/Overlay/Overlay";
+import { GUITAR } from "utils/constants";
+import { formatPrice } from "utils/utils";
+import style from "./CartModal.module.scss";
+
+type ModalType = "add" | "delete";
+
+type CartModalProps = {
+  componentRef: MutableRefObject<HTMLDivElement | null>;
+  guitar: Guitar;
+  modalType: ModalType;
+  onModalClose: () => void;
+};
+
+export const CartModal = (props: CartModalProps) => {
+  const { componentRef, guitar, modalType, onModalClose } = props;
+  const { previewImg, name, vendorCode, type, stringCount, price } = guitar;
+
+  const isModalAdd = modalType === "add";
+  const heading = isModalAdd ? "Добавить товар в корзину" : "Удалить этот товар?";
+  const guitarType = GUITAR[type].label;
+
+  return (
+    <Overlay>
+      <div className={style.component} ref={componentRef}>
+        <ButtonClose onClick={onModalClose} />
+
+        <h2 className={style.heading}>{heading}</h2>
+
+        <div className={style.infoWrapper}>
+          <img className={style.image} width="67" height="137" alt="Гитара" src={`/${previewImg}`} />
+          <div className={style.info}>
+            <p className={style.nameText}>Гитара {name}</p>
+            <p className={style.text}>Артикул: {vendorCode}</p>
+            <p className={style.text}>
+              {guitarType}, {stringCount} струнная
+            </p>
+            <p className={style.priceText}>Цена: {formatPrice(price)} ₽</p>
+          </div>
+        </div>
+
+        {isModalAdd ? (
+          <Button className={style.buttonAdd} color="red">
+            Добавить в корзину
+          </Button>
+        ) : (
+          <div className={style.buttonsWrapper}>
+            <Button color="white-red">Удалить товар</Button>
+            <Button color="white-black">Продолжить покупки</Button>
+          </div>
+        )}
+      </div>
+    </Overlay>
+  );
+};
--- src/components/PageScreens/CatalogScreen/Catalog/GuitarCard/GuitarCard.tsx
+++ src/components/PageScreens/CatalogScreen/Catalog/GuitarCard/GuitarCard.tsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
+import useModal from "hooks/useModal";
 import { Guitar } from "types/guitar";
 import { Button } from "components/Common/Button/Button";
 import { ButtonLink } from "components/Common/ButtonLink/ButtonLink";
 import { Icon } from "components/Common/Icon/Icon";
 import { RatingStars } from "components/Common/RatingStars/RatingStars";
+import { CartModal } from "components/Modals/CartModal/CartModal";
 import { formatPrice } from "utils/utils";
 import style from "./GuitarCard.module.scss";
 
@@ -13,26 +16,42 @@ type GuitarCardProps = {
 export const GuitarCard = ({ guitar }: GuitarCardProps) => {
   const { previewImg, rating, comments, name, price } = guitar;
 
+  const modalAddRef = useRef<HTMLDivElement | null>(null);
+  const [isModalAddOpen, setIsModalAddOpen] = useModal({ componentRef: modalAddRef, isClickCapture: true });
+
+  const onAddButtonClick = () => setIsModalAddOpen.openModal();
+
   return (
-    <div className={style.component}>
-      <img className={style.image} width="68" height="190" alt="Гитара" src={`/${previewImg}`} />
+    <>
+      {isModalAddOpen && (
+        <CartModal
+          componentRef={modalAddRef}
+          guitar={guitar}
+          modalType="add"
+          onModalClose={setIsModalAddOpen.closeModal}
+        />
+      )}
 
-      <div className={style.infoWrapper}>
-        <RatingStars rating={rating} commentCount={comments.length} />
-        <div className={style.info}>
-          <p className={style.infoTitle}>{name}</p>
-          <p className={style.infoPrice}>{formatPrice(price)} ₽</p>
-        </div>
+      <div className={style.component}>
+        <img className={style.image} width="68" height="190" alt="Гитара" src={`/${previewImg}`} />
 
-        <div className={style.buttonsWrapper}>
-          <ButtonLink className={style.button} to={"#"}>
-            Подробнее
-          </ButtonLink>
-          <Button className={style.button} color="red" icon={<Icon iconName="cartAlt" />}>
-            Купить
-          </Button>
+        <div className={style.infoWrapper}>
+          <RatingStars rating={rating} commentCount={comments.length} />
+          <div className={style.info}>
+            <p className={style.infoTitle}>{name}</p>
+            <p className={style.infoPrice}>{formatPrice(price)} ₽</p>
+          </div>
+
+          <div className={style.buttonsWrapper}>
+            <ButtonLink className={style.button} to={"#"}>
+              Подробнее
+            </ButtonLink>
+            <Button className={style.button} color="red" icon={<Icon iconName="cartAlt" />} onClick={onAddButtonClick}>
+              Купить
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
