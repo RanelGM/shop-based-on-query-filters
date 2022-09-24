3ce03d565c12ea8d24608beaa1d8798dfcc28265
RanelGM <mr.rr.coon@gmail.com>
2022-09-24 21:09:07 +0300

    Add comment sort

--- src/store/guitar/selectors.ts
+++ src/store/guitar/selectors.ts
@@ -3,8 +3,26 @@ import { Price, Option } from "types/common";
 import { Guitar } from "types/guitar";
 
 export const getGuitars = (state: RootReducerState): Guitar[] | null => state.guitar.guitars;
-export const getGuitar = (state: RootReducerState): Guitar | null => state.guitar.guitar;
+
+export const getGuitar = (state: RootReducerState): Guitar | null => {
+  const guitar = state.guitar.guitar;
+
+  if (!guitar) {
+    return null;
+  }
+
+  // Сортирует комментарии по дате создания - от самых новых к старым
+  return {
+    ...guitar,
+    comments: guitar.comments
+      .slice()
+      .sort((first, second) => new Date(first.createAt).getTime() - new Date(second.createAt).getTime())
+      .reverse(),
+  };
+};
+
 export const getPrice = (state: RootReducerState): Price => state.guitar.price;
+
 export const getSearchOptions = (state: RootReducerState): Option[] | null => {
   const searchGuitars = state.guitar.search;
 
